4ca06450f2c5ba9295b1e660379c0b0b37a0ac53
Роман Савощик <tweakice@gmail.com>
2023-03-24 14:22:24 +0600

    add processing

--- src/processor.ts
+++ src/processor.ts
@@ -1,8 +1,8 @@
-import { BatchHandlerContext, BatchProcessorItem, EvmBatchProcessor, EvmBlock } from '@subsquid/evm-processor'
-import { LogItem } from '@subsquid/evm-processor/lib/interfaces/dataSelection'
+import { BatchBlock, BatchHandlerContext, BatchProcessorItem, EvmBatchProcessor, EvmBlock, LogHandlerContext } from '@subsquid/evm-processor'
+import { AddLogItem, LogItem, TransactionItem } from '@subsquid/evm-processor/lib/interfaces/dataSelection'
 import { Store, TypeormDatabase } from '@subsquid/typeorm-store'
 import { lookupArchive } from '@subsquid/archive-registry'
-import { In } from 'typeorm';
+import { EntityManager, In } from 'typeorm';
 import { SFC_OLD_ADDR, SFC_2_ADDR, SFCR_2_ADDR, SFCR_ADDR, DEPLOYMENT_BLOCK } from './constants';
 import { User, Transfer, Token, Event } from './model';
 
@@ -30,96 +30,107 @@ const processor = new EvmBatchProcessor()
     },
 })
 
-type Item = BatchProcessorItem<typeof processor>
-type Ctx = BatchHandlerContext<Store, Item>
 // For topics and decode thoose event
 const erc20Transfer = erc20.events.Transfer;
+const tokenName: Map<string, string> = new Map([
+  [SFC_OLD_ADDR, 'SFC Old'],
+  [SFC_2_ADDR, 'SFC 2'],
+  [SFCR_ADDR, 'SFCR'],
+  [SFCR_2_ADDR, 'SFCR 2'],
+])
 
 processor.run(database, async (ctx) => {
-  let transfersData: TransferEventData[] = []
+  const events: Set<Event> = new Set();
 
   for (let block of ctx.blocks) {
     for (let item of block.items) {
       if (item.kind !== 'evmLog') continue
 
       if (item.evmLog.topics[0] === erc20Transfer.topic) {
-        transfersData.push(handleTransfer(ctx, block.header, item))
-      }     
+        if (item.address === SFC_OLD_ADDR) {
+          events.add(
+            await handleTransferEvent(ctx, item, block, SFC_OLD_ADDR)
+          )
+          
+        } else if (item.address === SFC_2_ADDR) {
+          events.add(
+            await handleTransferEvent(ctx, item, block, SFC_2_ADDR)
+          )
+        } else if (item.address === SFCR_ADDR) {
+          events.add(
+            await handleTransferEvent(ctx, item, block, SFCR_ADDR)
+          )
+
+        } else if (item.address === SFCR_2_ADDR) {
+          events.add(
+            await handleTransferEvent(ctx, item, block, SFCR_2_ADDR)
+          )
+        }
+      }
     }
   }
-
-  await saveTransfers(ctx, transfersData)
+  await ctx.store.save([...events]);
 })
 
-async function saveTransfers(ctx: Ctx, transfersData: TransferEventData[]) {
-    let accountIds = new Set<string>()
-    for (let t of transfersData) {
-        accountIds.add(t.from)
-        accountIds.add(t.to)
+async function handleTransferEvent(
+  ctx: BatchHandlerContext<Store, AddLogItem<LogItem<false> | TransactionItem<false>, LogItem<{ evmLog: { topics: true; data: true; }; transaction: { hash: true; }; }>>>, 
+  item: { evmLog: { id: string; address: string; index: number; transactionIndex: number; data: string; topics: string[]; }; address: string; transaction: { id: string; hash: string; to?: string | undefined; index: number; }; kind: "evmLog"; },
+  block: BatchBlock<AddLogItem<LogItem<false> | TransactionItem<false>, LogItem<{ evmLog: { topics: true; data: true; }; transaction: { hash: true; }; }>>>,
+  tokenId: string
+  ): Promise<Event> {
+
+  const findOrCreateUser = async (id: string): Promise<User> => {
+    const mgr: EntityManager = await ctx.store["em"]();
+    const repo = mgr.getRepository(User);
+  
+    let user = await repo.findOneBy({ id });
+    if (!user) {
+      user = new User({ id });
+      await repo.save(user);
     }
+    return user;
+  };
 
-    let accounts = await ctx.store
-        .findBy(Account, {id: In([...accountIds])})
-        .then((q) => new Map(q.map((i) => [i.id, i])))
-
-    let transfers: Transfer[] = []
-
-    for (let t of transfersData) {
-        let {id, blockNumber, timestamp, txHash, amount} = t
-
-        let from = getAccount(accounts, t.from)
-        let to = getAccount(accounts, t.to)
-
-        transfers.push(
-            new Transfer({
-                id,
-                blockNumber,
-                timestamp,
-                txHash,
-                from,
-                to,
-                amount,
-            })
-        )
-    }
-
-    await ctx.store.save(Array.from(accounts.values()))
-    await ctx.store.insert(transfers)
-}
-
-interface TransferEventData {
-  token: Token
-  from: User
-  to: User
-  amount: bigint
-  txHash: string
-}
-
-function handleTransfer(
-    ctx: Ctx,
-    block: EvmBlock,
-    item: LogItem<{evmLog: {topics: true; data: true}; transaction: {hash: true}}>
-): TransferEventData {
+  const mgr: EntityManager = await ctx.store["em"]();
+  const tokenRepo = mgr.getRepository(Token);
 
   let event = new Event({
     id: `${item.transaction.hash}-${item.evmLog.index.toString()}`,
-    createdAt: new Date(block.timestamp)
+    createdAt: new Date(block.header.timestamp)
   });
 
   let user: User | null;
+  let token: Token | null;
 
-  if (item.address === SFC_OLD_ADDR) {
-    
+  
+  const { from, to, value } = erc20Transfer.decode(item.evmLog);
+  
+  token = await tokenRepo.findOneBy({
+    id: tokenId
+  });
+
+  if (!token) {
+    token = new Token ({
+      id: tokenId,
+      name: tokenName.get(tokenId),
+    });
   }
-    
-}
+  
+  user = await findOrCreateUser(from);
 
-function getAccount(m: Map<string, Account>, id: string): Account {
-    let acc = m.get(id)
-    if (acc == null) {
-        acc = new Account()
-        acc.id = id
-        m.set(id, acc)
-    }
-    return acc
+  let transfer: Transfer = new Transfer({
+    token: token.id,
+    from: user.id,
+    to: (await findOrCreateUser(to)).id,
+    amount: value.toBigInt(),
+    txHash: item.transaction.hash,
+  });
+  // push transfer linked to this token
+  token.transfers.push(transfer);
+  await tokenRepo.save(token);
+  // set transfer and user to eventLog column of Event entity
+  event.eventLog = transfer;
+  event.user = user;
+    
+  return event;
 }
