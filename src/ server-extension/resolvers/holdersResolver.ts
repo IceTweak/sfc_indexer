15345c5fc896ac9a08fa0ad993f822c99080af62
Роман Савощик <tweakice@gmail.com>
2023-04-12 16:54:57 +0600

    finish resolver

--- src/ server-extension/resolvers/holdersResolver.ts
+++ src/ server-extension/resolvers/holdersResolver.ts
@@ -1,13 +1,14 @@
 import { Field, ObjectType, Query, Resolver } from "type-graphql";
+import { ethers } from "ethers";
 import { EntityManager } from "typeorm";
 import { User } from "../../model/generated/user.model";
 
 @ObjectType()
 export class Holders {
-  @Field(() => BigInt, { nullable: false })
-  sfcHolders!: BigInt;
-  @Field(() => BigInt, { nullable: false })
-  sfcrHolders!: BigInt
+  @Field(() => Number, { nullable: false })
+  sfcHolders!: number;
+  @Field(() => Number, { nullable: false })
+  sfcrHolders!: number
 
   constructor(props: Partial<Holders>) {
     Object.assign(this, props);
@@ -21,18 +22,20 @@ export class HoldersResolver {
   @Query(() => Holders)
   async getHolders(): Promise<Holders> {
     const manager = await this.tx();
+    const zeroAddr = ethers.constants.AddressZero;
     const sfcQuery = `
-    SELECT COUNT(*) FROM "user" WHERE id != '0x0000000000000000000000000000000000000000' AND sfc_old > 0 AND sfc2 > 0;
+    SELECT COUNT(*) FROM "user" WHERE id != '${zeroAddr}' AND sfc_old > 0 AND sfc2 > 0;
     `;
     const sfcrQuery = `
-    SELECT COUNT(*) FROM "user" WHERE id != '0x0000000000000000000000000000000000000000' AND sfcr > 0 AND sfcr2 > 0;
+    SELECT COUNT(*) FROM "user" WHERE id != '${zeroAddr}' AND sfcr > 0 AND sfcr2 > 0;
     `;
 
     const sfcHoldersCount = await manager.getRepository(User).query(sfcQuery);
     const sfcrHoldersCount = await manager.getRepository(User).query(sfcrQuery);
-    return {
-        sfcHolders: sfcHoldersCount,
-        sfcrHolders: sfcrHoldersCount,
-    };
+    const holders = new Holders({
+      sfcHolders: sfcHoldersCount,
+      sfcrHolders: sfcrHoldersCount,
+    });
+    return holders;
   }
 }
